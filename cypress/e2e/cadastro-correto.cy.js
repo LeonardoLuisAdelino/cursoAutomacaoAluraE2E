6ad4ddb55395254f614690f00573dc23140b4414
LeonardoLuisAdelino <luislopesleonardo@gmail.com>
2024-09-17 17:12:00 -0300

    atualizando projeto da automacao

---
+++ cypress/e2e/cadastro-correto.cy.js
@@ -0,0 +1,34 @@
+describe('página de cadastro', () => {
+  it('DEve preencher os campos do formulario corretamente para cadastrar um novo usuário', () => {
+    cy.visit('https://adopet-frontend-cypress.vercel.app/');
+    cy.get('[data-test="register-button"]').click();
+    cy.get('[data-test="input-name').type('Ana de Jesus');
+    cy.get('[data-test="input-email"]').type('ana@email.com');
+    cy.get('[for="pass-create"]').type('Senha123');
+    cy.get('[for="pass-confirm"]').type('Senha123');
+    cy.get('[data-test="submit-button"]').click();
+    
+  })
+  /*
+  it('deve carregar a página corretamente e clicar no botão ‘Ver pets disponíveis para adoção”', () => {
+    cy.visit('https://adopet-frontend-cypress.vercel.app/');
+    cy.get('.button').click()     
+   
+});
+
+  it("entrar na adopet e testar o botao reader", () =>{
+    cy.visit('https://adopet-frontend-cypress.vercel.app/');
+    cy.get('.header__home').click();
+  })
+  it('entrar no login do adopet', () =>{
+    cy.visit('https://adopet-frontend-cypress.vercel.app/login')
+  })
+  it('entrar no login do adopet', () =>{
+    cy.visit('https://adopet-frontend-cypress.vercel.app/home')
+  })
+  it('entrar no login do adopet', () =>{
+    cy.visit('https://adopet-frontend-cypress.vercel.app/home')
+    cy.get('.header__message').click();
+  })
+  */
+})
